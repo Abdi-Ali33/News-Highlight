8c94a8dfcac1d0d826195bfd63143d4df5214f08
Abdi Ali <alikune33@gmail.com>
2022-05-01 16:39:56 +0300

    create manage.py file

---
+++ manage.py
@@ -0,0 +1,23 @@
+from email.policy import default
+from app import create_app
+from flask_script import Manager, Server
+from decouple import config
+
+app = create_app(config('ENV', default="development"))
+
+manager = Manager(app)
+manager.add_command('server', Server)
+
+manager.add_command('server', Server)
+
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == '__main__':
+    manager.run()
